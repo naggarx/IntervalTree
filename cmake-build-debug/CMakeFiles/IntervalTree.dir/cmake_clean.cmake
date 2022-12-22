8a94a41cd2fd36c793162753a31dba0b9fc5e3ed
naggarx <72861717+naggarx@users.noreply.github.com>
2022-12-22 12:17:58 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/IntervalTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IntervalTree.dir/main.cpp.obj"
+  "IntervalTree.exe"
+  "IntervalTree.exe.manifest"
+  "IntervalTree.pdb"
+  "libIntervalTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IntervalTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
